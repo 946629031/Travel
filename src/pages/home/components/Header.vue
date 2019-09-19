be56f985394ae14efc42598b30eb16922df39157
threeki <946629031@qq.com>
2019-09-19 08:49:24 +0800

    7-2 Vue项目首页 - iconfont 的使用和代码优化和代码优化 2019-9-19

--- src/pages/home/components/Header.vue
+++ src/pages/home/components/Header.vue
@@ -1,8 +1,15 @@
 <template>
   <div class="header">
-    <div class="header-left">返回</div>
-    <div class="header-input">输入城市/景点/游玩主题</div>
-    <div class="header-right">城市</div>
+    <div class="header-left">
+      <div class="iconfont">&#xeb99;</div>
+    </div>
+    <div class="header-input">
+      <span class="iconfont">&#xeb9c;</span>
+      输入城市/景点/游玩主题</div>
+    <div class="header-right">
+      城市
+      <span class="iconfont arrow-icon">&#xe65c;</span>
+    </div>
   </div>
 </template>
 
@@ -14,10 +21,11 @@ export default {
 
 <!-- 添加“scoped”属性以仅将CSS限制到此组件 -->
 <style lang="stylus" scoped>
+    @import '../../../assets/styles/varibles.styl' // 引入 css变量文件
     .header
         display: flex
         line-height: .86rem
-        background: #00bcd4
+        background: $bgColor // 使用css变量
         color: #fff
         font-size: 16px
         .header-left
@@ -28,10 +36,12 @@ export default {
             background: #fff
             height: .64rem
             line-height: .64rem
-            margin: .12rem 0 0 .2rem
+            margin: .12rem .1rem 0 .1rem
             border-radius: .1rem
             color: #ccc
         .header-right
             width: 1.24rem
             float: right
+            .arrow-icon
+              margin-left: -.1rem
 </style>
