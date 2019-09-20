d8183befbe3b03327de21bb1f871fc6984ba241b
threeki <946629031@qq.com>
2019-09-20 20:44:44 +0800

    7-4 Vue项目首页 - 图标区域页面布局 2019-9-20

--- src/pages/home/Home.vue
+++ src/pages/home/Home.vue
@@ -2,18 +2,20 @@
   <div>
     <home-header></home-header>
     <home-swiper></home-swiper>  <!-- 使用 HomeSwiper 组件 -->
-    <div>test</div>
+    <home-icons></home-icons>
   </div>
 </template>
 
 <script>
 import HomeHeader from './components/Header'
 import HomeSwiper from './components/Swiper'
+import HomeIcons from './components/Icons'
 export default {
   name: 'Home',
   components: {
     HomeHeader,
-    HomeSwiper // 将组件HomeSwiper，注册到局部组件
+    HomeSwiper, // 将组件HomeSwiper，注册到局部组件
+    HomeIcons
   }
 }
 </script>
---
+++ src/pages/home/components/Icons.vue
@@ -0,0 +1,94 @@
+<template>
+    <div class="icons">
+      <div class="icon">
+        <div class="img-wrapper">
+          <img src="https://imgs.qunarzz.com/piao/fusion/1803/95/f3dd6c383aeb3b02.png">
+        </div>
+        <div class="keywords">景点门票</div>
+      </div>
+      <div class="icon">
+        <div class="img-wrapper">
+          <img src="https://imgs.qunarzz.com/piao/fusion/1803/95/f3dd6c383aeb3b02.png">
+        </div>
+        <div class="keywords">景点门票</div>
+      </div>
+      <div class="icon">
+        <div class="img-wrapper">
+          <img src="https://imgs.qunarzz.com/piao/fusion/1803/95/f3dd6c383aeb3b02.png">
+        </div>
+        <div class="keywords">景点门票</div>
+      </div>
+      <div class="icon">
+        <div class="img-wrapper">
+          <img src="https://imgs.qunarzz.com/piao/fusion/1803/95/f3dd6c383aeb3b02.png">
+        </div>
+        <div class="keywords">景点门票</div>
+      </div>
+      <div class="icon">
+        <div class="img-wrapper">
+          <img src="https://imgs.qunarzz.com/piao/fusion/1803/95/f3dd6c383aeb3b02.png">
+        </div>
+        <div class="keywords">景点门票</div>
+      </div>
+      <div class="icon">
+        <div class="img-wrapper">
+          <img src="https://imgs.qunarzz.com/piao/fusion/1803/95/f3dd6c383aeb3b02.png">
+        </div>
+        <div class="keywords">景点门票</div>
+      </div>
+      <div class="icon">
+        <div class="img-wrapper">
+          <img src="https://imgs.qunarzz.com/piao/fusion/1803/95/f3dd6c383aeb3b02.png">
+        </div>
+        <div class="keywords">景点门票</div>
+      </div>
+      <div class="icon">
+        <div class="img-wrapper">
+          <img src="https://imgs.qunarzz.com/piao/fusion/1803/95/f3dd6c383aeb3b02.png">
+        </div>
+        <div class="keywords">景点门票</div>
+      </div>
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'Icons'
+}
+</script>
+
+<style lang="stylus" scoped>
+  .icons
+    width: 100%
+    padding-bottom: 50%
+    .icon
+      width: 25%
+      padding-bottom: 25%
+      height: 0
+      float: left
+      position: relative
+      border: 1px solid #ccc // 分界线
+      background: red
+      .img-wrapper
+        position: relative
+        padding-bottom: 100%
+        height: 0
+        img
+          width: 60%
+          position:absolute
+          left: 0
+          right: 0
+          top:0
+          bottom: .44rem
+          margin: auto
+      .keywords
+        font-size: .22rem
+        height: .44rem
+        line-height: .44rem
+        position: absolute
+        top: 65%
+        bottom: 0
+        left: 0
+        right: 0
+        margin: auto
+</style>
