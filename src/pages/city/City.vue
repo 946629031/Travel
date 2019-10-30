f1aec5ee21718f9aadb9998c59e332d3cf438a92
threeki <946629031@qq.com>
2019-10-30 15:37:10 +0800

    8-5 Vue项目 <城市选择页> - 页面的动态数据渲染 2019-10-30

--- src/pages/city/City.vue
+++ src/pages/city/City.vue
@@ -2,23 +2,46 @@
   <div>
     <city-header></city-header>
     <city-search></city-search>
-    <city-list></city-list>
-    <city-alphabet></city-alphabet>
+    <city-list :cities='cities' :hot='hotCities'></city-list>
+    <city-alphabet :cities='cities'></city-alphabet>
   </div>
 </template>
 
 <script>
+import axios from 'axios'
 import CityHeader from './components/Header'
 import CitySearch from './components/Search'
 import CityList from './components/List'
 import CityAlphabet from './components/Alphabet'
 export default{
   name: 'City',
+  data () {
+    return {
+      hotCities: [],
+      cities: {}
+    }
+  },
   components: {
     CityHeader,
     CitySearch,
     CityList,
     CityAlphabet
+  },
+  methods: {
+    getCityInfo () {
+      axios.get('/api/city.json')
+        .then(this.handleGetCityInfoSuccess)
+    },
+    handleGetCityInfoSuccess (response) {
+      let res = response.data
+      if (res.ret && res.data) {
+        this.cities = res.data.cities
+        this.hotCities = res.data.hotCities
+      }
+    }
+  },
+  mounted () {
+    this.getCityInfo()
   }
 }
 </script>
