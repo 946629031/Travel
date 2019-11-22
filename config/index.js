99343d8851a9aecae632fb86229861d709989523
threeki <946629031@qq.com>
2019-11-22 15:26:19 +0800

    10-4 异步组件实现按需加载 2019-11-22

--- config/index.js
+++ config/index.js
@@ -12,7 +12,10 @@ module.exports = {
     assetsPublicPath: '/',
     proxyTable: {   // 代理转发
       '/api': {
-        target: 'http://localhost:80'
+        target: 'http://localhost:8080',
+        pathRewrite: {
+          '^/api': '/static/data_demo'
+        }
       }
     },
 
--- src/router/index.js
+++ src/router/index.js
@@ -1,8 +1,38 @@
+// import Vue from 'vue'
+// import Router from 'vue-router'
+// import Home from '@/pages/home/Home'
+// import City from '@/pages/city/City'
+// import Detail from '@/pages/detail/Detail'
+
+// Vue.use(Router)
+
+// export default new Router({
+//   routes: [
+//     {
+//       path: '/',
+//       name: 'Home',
+//       component: Home
+//     }, {
+//       path: '/city',
+//       name: 'City',
+//       component: City
+//     }, {
+//       path: '/detail/:id',
+//       name: 'Detail',
+//       component: Detail
+//     }
+//   ],
+//   scrollBehavior (to, from, savedPosition) {
+//     return { x: 0, y: 0 }
+//   }
+// })
+
+// // 以下是 异步组件实现按需加载
 import Vue from 'vue'
 import Router from 'vue-router'
-import Home from '@/pages/home/Home'
-import City from '@/pages/city/City'
-import Detail from '@/pages/detail/Detail'
+// import Home from '@/pages/home/Home'
+// import City from '@/pages/city/City'
+// import Detail from '@/pages/detail/Detail'
 
 Vue.use(Router)
 
@@ -11,15 +41,15 @@ export default new Router({
     {
       path: '/',
       name: 'Home',
-      component: Home
+      component: () => import('@/pages/home/Home')
     }, {
       path: '/city',
       name: 'City',
-      component: City
+      component: () => import('@/pages/city/City')
     }, {
       path: '/detail/:id',
       name: 'Detail',
-      component: Detail
+      component: () => import('@/pages/detail/Detail')
     }
   ],
   scrollBehavior (to, from, savedPosition) {
